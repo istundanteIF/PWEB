a9431b7b7b216464697fceec744e200450208c00
istundanteIF <110903531+istundanteIF@users.noreply.github.com>
2022-11-20 00:19:10 -0300

    0410 - Quinto commit

---
+++ pwebdir-main/biblioteca/src/main/java/br/edu/ifgoiano/BibliotecaApplication.java
@@ -0,0 +1,22 @@
+package br.edu.ifgoiano;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.edu.ifgoiano.entidade.Livro;
+import br.edu.ifgoiano.repository.LivroRepository;
+
+@SpringBootApplication
+public class BibliotecaApplication implements CommandLineRunner {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+}
